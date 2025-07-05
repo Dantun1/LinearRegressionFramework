7ddf755b24c3c596ca743201f7f71c0bff21cc17
Dantun1 <daniel@tunyan.com>
2025-07-05 10:27:34 +0400

    .analysis() method implemented in LR model class. Basic RegressionAnalysis() class.

--- stats_framework/models/linear_regression/analysis.py
+++ stats_framework/models/linear_regression/analysis.py
@@ -1,5 +1,48 @@
-#TODO create regression analysis object that is created when model is trained, contains metrics, visualisations, conf
-# intervals, prediction intervals, etc all in one class.
+import numpy as np
+# from stats_framework.models.linear_regression.model import LinearRegression
+from stats_framework.data.linear_regression_dataset import LinearRegressionDataset
+from stats_framework.shared_utilities.exceptions.model_errors import NotFittedError
+
+#TODO:
+# -Validation of dimensions of model coefficients and dataset
+# -Implement r_squared method
 
 class RegressionAnalysis:
-    pass
+
+    def __init__(self, model, **kwargs):
+        self.model = model
+
+        dataset = kwargs.get("dataset")
+        if dataset is None:
+            print("No dataset provided, using training dataset")
+            dataset = self.model.dataset
+        elif not isinstance(dataset, LinearRegressionDataset):
+            raise TypeError("dataset must be of type LinearRegressionDataset")
+
+        self.dataset = dataset
+        self.predictions = self._get_predictions()
+
+    def r_squared(self):
+        predictions = self.predictions
+        response = self.dataset.response
+
+        tss = sum((response - response.mean()) ** 2)
+        rss = sum((response - predictions)**2)
+
+        return (tss - rss)/ rss
+
+
+
+    def _get_predictions(self) -> np.ndarray:
+        features_matrix = self.dataset.features.to_numpy()
+
+        if self.model.coefficients is None:
+            raise NotFittedError("model must be fitted before predictions can be made")
+
+        coefficients = np.array(self.model.coefficients)
+
+        predictions = features_matrix * coefficients
+
+        return predictions
+
+
--- stats_framework/models/linear_regression/model.py
+++ stats_framework/models/linear_regression/model.py
@@ -1,17 +1,20 @@
+from typing import Optional
+
 from stats_framework.data import LinearRegressionDataset
 from stats_framework.models.base_model import BaseModel
-import numpy as np
+from stats_framework.models.linear_regression.analysis import RegressionAnalysis
+from stats_framework.shared_utilities.exceptions.model_errors import NotFittedError
+
 
 #TODO:
 # Implement LinearRegression Model that calculates coefficients(returned in fit), predicts on a np array of features.
-#TODO:
-# Implement .analysis() method that returns a RegressionAnalysis object.
-
-
 
 class LinearRegression(BaseModel):
     """
     """
+    def __init__(self, dataset: LinearRegressionDataset):
+        super(LinearRegression, self).__init__(dataset)
+        self.coefficients: Optional[list[float]] = None
 
     @BaseModel.dataset.setter
     def dataset(self, dataset):
@@ -20,15 +23,47 @@ class LinearRegression(BaseModel):
         if not isinstance(dataset, LinearRegressionDataset):
             raise TypeError("The provided dataset must be a LinearRegressionDataset")
 
-    def fit(self):
-        return 10
-
+    def fit(self) -> None:
+        return
 
     def predict(self):
         return 0
 
-    def analysis(self):
-        pass
+    @property
+    def is_fitted(self):
+        return self.coefficients is not None
+
+
+    def analysis(self, **kwargs) -> RegressionAnalysis:
+        """
+        This method tries to return a RegressionAnalysis object giving access to statistical metrics and visualizations.
+
+        The user may input a different LinearRegressionDataset to the one the model has been trained on,
+        if getting analysis fails due to the invalid type of inputted dataset,
+        return the analysis on the pre-validated training dataset.
+
+        Raises:
+            NotFittedError if the coefficients are not fitted when RegressionAnalysis object is instantiated.
+
+
+        Return a regression analysis object
+        """
+
+        try:
+            if kwargs.get("dataset") is None:
+                regression_analysis = RegressionAnalysis(self)
+            else:
+                try:
+                    regression_analysis = RegressionAnalysis(self, dataset=kwargs.get("dataset"))
+                except TypeError as e:
+                    print(f"Error analysing model with inputted data:{e}")
+                    regression_analysis = RegressionAnalysis(self)
+
+        except NotFittedError as e:
+            raise NotFittedError(e)
+
+        return regression_analysis
+
 
 
 
